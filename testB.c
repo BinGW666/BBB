9f5b33c5949bf4f92b5c8799a490c5420b1dd0de
BinGW666 <a18685039778@163.com>
2020-11-26 23:06:52 +0800

    new commit

---
+++ testB.c
@@ -0,0 +1,25 @@
+   #include"sort.h"
+     #include<stdio.h>
+       #include<stdlib.h>
+        
+	  
+	     int main(int argc, char **argv)
+	   {
+		             SQ*sq = (SQ*)malloc(sizeof(SQ));
+		               initsort(sq);
+		                creatsort(sq);
+		                printf("原始数据为:\n");
+			     printsort(*sq)；
+				           printf("冒泡排序后:\n");
+			           bubblesort(*sq);
+			            printf("插入排序后:\n");
+			             insertsort(*sq);
+			              printf("选择排序后:\n");
+			               sqlqctsort(*sq);
+			                printf("快速排序后:\n");
+			                 QuickSort(*sq);
+				         printf("原始数据为:\n");
+				  
+					            return 0;
+				   }
+				    
